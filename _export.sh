8558bf1de56b785d2430075b1c5e1ef3b29adcad
Taichi <taichi@curipha.jp>
2014-05-05 14:02:34 +0900

    Add _export.sh

---
+++ _export.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Userscripts extractor
+# * Extract *.user.js from Firefox profile directory
+
+FXPROFILE=${USERPROFILE}\\AppData\\Roaming\\Mozilla\\Firefox\\Profiles
+USERJSEXT=*.user.js
+
+
+# Remove current user.js
+rm ${USERJSEXT}
+
+# Copy userscripts from Firefox's user profile directory
+find "${FXPROFILE}" -type f -iname "${USERJSEXT}" -ipath "*/scriptish_scripts/*" -exec cp {} . \;
+
+
+# Remove userscripts not developed by me
+rm autopagerize@httpswdyhyuto.user.js
+
+
+# Change linefeed code to LF only
+for file in `ls -1 ${USERJSEXT}`; do
+  echo Processing "${file}"...
+
+  tr -d "\r" < ${file} > ${file}.lf
+  mv ${file}.lf ${file}
+done
+
+
+# Update mode
+chmod 0644 ${USERJSEXT}
+
+
