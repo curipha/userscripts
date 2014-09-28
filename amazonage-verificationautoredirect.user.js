af22b4cdbb3dffdbf9f56cc11c420cb4a56361db
Taichi <taichi@curipha.jp>
2014-09-28 14:42:05 +0900

    Support messages on search result page

--- amazonage-verificationautoredirect.user.js
+++ amazonage-verificationautoredirect.user.js
@@ -1,21 +1,41 @@
 // ==UserScript==
-// @id             Amazon age-verification autoredirect
 // @name           Amazon age-verification autoredirect
-// @version        0.0.5
 // @namespace      curipha
-// @author         curipha
-// @description    Automatic click an "I'm over 18." link at amazon.co.jp.
+// @description    Click the "I'm over 18" link automatically at amazon.co.jp.
 // @include        http://www.amazon.co.jp/*
-// @run-at         document-end
+// @include        https://www.amazon.co.jp/*
+// @version        0.1.2
 // @grant          none
 // ==/UserScript==
 
 (function() {
-  var hrefs = document.getElementsByTagName('a');
-  for (var a of hrefs) {
-    if (/black\-curtain\-redirect\.html/.test(a.href)) {
-      location.href = a.href;
-      return;
+  var pattern = /black\-curtain\-redirect\.html/;
+  var t = 0;
+
+  var clickit = function(elem) {
+    var hrefs = elem.getElementsByTagName('a');
+
+    for (var a of hrefs) {
+      if (pattern.test(a.href)) {
+        location.href = a.href;
+        return;
+      }
     }
-  }
+  };
+  var clickit_wrap = function(mr) {
+    if (t) return;
+    ｔ = setTimeout(function() {
+      for (var mre of mr) {
+        if (mre.target.id !== 'nav_flyout_anchor') continue;
+
+        clickit(mre.target);
+      }
+      t = 0;
+    }, 120);
+  };
+
+  clickit(document);
+
+  var mo = new MutationObserver(clickit_wrap);
+  mo.observe(document.getElementById('nav-cross-shop'), { childList: true, subtree: true });
 })();
