db9b1d12c2f5e5afc773096920a096d0de455ea5
Taichi <taichi@curipha.jp>
2014-09-28 23:59:31 +0900

    Submit button is just unused
    If submit button completely removes from HTML, a warning message of
    redirect will be shown. Therefore submit button is hidden by style.

--- addgooglesitesearch.user.js
+++ addgooglesitesearch.user.js
@@ -6,7 +6,7 @@
 // @include        https://*
 // @exclude        http://*.google.tld/*
 // @exclude        https://*.google.tld/*
-// @version        0.0.7
+// @version        0.0.8
 // @grant          none
 // ==/UserScript==
 
@@ -22,11 +22,11 @@
 
   var css =
   // "z-index" should be less than 255 because an indicator of Autopagerize is at 256.
-    '#userjs-add_google { background: #ddd; padding: 10px; line-height: 1; min-width: 250px; border: 2px solid #ccc; position: fixed; top: 0; right: 0; z-index: 255; } '+
+    '#userjs-add_google { background: #ddd; padding: 10px; line-height: 1; min-width: 200px; border: 2px solid #ccc; position: fixed; top: 0; right: 0; z-index: 255; } '+
     '#userjs-add_google form { margin: 0 !important; } '+
-    '#userjs-add_google input { font-size: 14px; padding: 0.3em !important; display: inline !important; } '+
-    '#userjs-add_google input[type="text"] { width: 12em; } '+
-    '#userjs-add_google_del { color: #000 !important; font: bold 20px sans-serif !important; cursor: pointer !important; }';
+    '#userjs-add_google input { font: 18px monospace !important; padding: 0.3em !important; display: inline !important; width: 14em !important; } '+
+    '#userjs-add_google input[type="submit"] { display: none !important; } '+
+    '#userjs-add_google_del { color: #000 !important; font: bold 24px sans-serif !important; margin-right: 8px !important; cursor: pointer !important; }';
   style.appendChild(document.createTextNode(css));
 
   var form = document.createElement('div');
@@ -40,8 +40,8 @@
   function init() {
     this.innerHTML =
       '<form action="https://www.google.com/search" method="get" accept-charset="UTF-8" target="_top">'+
-      '<span id="userjs-add_google_del">&times;</span>&nbsp;'+
-      '<input type="text" name="q">&nbsp;<input type="submit" value="Search">'+
+      '<span id="userjs-add_google_del">&times;</span>'+
+      '<input type="text" name="q"><input type="submit" value="Search">'+
       '<input type="hidden" name="as_sitesearch" value="' + document.domain + '">'+
       '<input type="hidden" name="safe" value="off">'+
       '</form>';
