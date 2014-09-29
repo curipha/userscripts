07efb2c717506616aeb1ea2c5ddb49d85c1b7ad5
Taichi <taichi@curipha.jp>
2014-09-30 00:17:02 +0900

    Support following result page

--- googlepersonalblocklist.user.js
+++ googlepersonalblocklist.user.js
@@ -4,7 +4,7 @@
 // @description    Remove sites from Google search results
 // @include        http://www.google.tld/search?*
 // @include        https://www.google.tld/search?*
-// @version        0.1.0
+// @version        0.2.0
 // @grant          none
 // ==/UserScript==
 
@@ -45,24 +45,35 @@
   var opacity = 0.2;
   var pattern = new RegExp('^https?:\/\/(?:' + block.join('|').replace(/([.\/])/g, '\\$1') + ')', 'i');
   var googler = /^https?:\/\/www\.google\.com\/url\?/i;
+  var t = 0;
 
-  var result = document.querySelectorAll('li.g');
-  for (var li of result) {
-    var a = li.querySelector('h3 a');
-    if (typeof(a.href) !== 'string') continue;
+  var blocker =function() {
+    if (t) return;
+    ｔ = setTimeout(function() {
+      var result = document.querySelectorAll('li.g');
+      for (var li of result) {
+        var a = li.querySelector('h3 a');
+        if (typeof(a.href) !== 'string') continue;
 
-    if (googler.test(a.href)) {
-      var alturi = li.querySelector('cite._Rm').textContent;
-      if (typeof(alturi) !== 'string') continue;
+        if (googler.test(a.href)) {
+          var alturi = li.querySelector('cite._Rm').textContent;
+          if (typeof(alturi) !== 'string') continue;
 
-      var uri = /^https?:\/\//.test(uri) ? alturi : 'http://' + alturi;
-    }
-    else {
-      var uri = a.href;
-    }
+          var uri = /^https?:\/\//.test(uri) ? alturi : 'http://' + alturi;
+        }
+        else {
+          var uri = a.href;
+        }
 
-    if (pattern.test(uri)) {
-      li.style.opacity = opacity;
-    }
-  }
+        if (pattern.test(uri)) li.style.opacity = opacity;
+      }
+
+      t = 0;
+    }, 240);
+  };
+
+  blocker();
+
+  var mo = new MutationObserver(blocker);
+  mo.observe(document.body, { childList: true, subtree: true });
 })();
