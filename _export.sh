f0a7cf7d9eb87ad0a96c896d1d51f0622403d1de
Taichi <taichi@curipha.jp>
2015-01-05 22:40:48 +0900

    Work correctly wherever a working directory is

--- _export.sh
+++ _export.sh
@@ -7,12 +7,16 @@ FXPROFILE="${USERPROFILE//\\/\/}/AppData/Roaming/Mozilla/Firefox/Profiles"
 USERJSEXT=.user.js
 
 
+# Change directory to the same level as this script
+cd `dirname "${0}"`
+
+
 # Remove current user.js
 rm -fv *"${USERJSEXT}"
 
+
 # Copy userscripts from Firefox's user profile directory
 find "${FXPROFILE}" -type f -iname "*${USERJSEXT}" -ipath "*/*_scripts/*" -exec cp {} . \;
-
 
 # Remove userscripts not developed by me
 rm -fv autopagerize*.user.js
@@ -25,7 +29,6 @@ for file in `ls -1 *"${USERJSEXT}"`; do
   tr -d "\r" < "${file}" > "${file}.lf"
   mv "${file}.lf" "${file}"
 done
-
 
 # Update mode
 chmod 0644 *"${USERJSEXT}"
