d02cac6106182c7af2bc0b45a18e12b6eb7a510f
Taichi <taichi@curipha.jp>
2015-02-15 02:03:25 +0900

    Remove trailing spaces when extracting userscripts

--- _export.sh
+++ _export.sh
@@ -26,7 +26,7 @@ rm -fv autopagerize*.user.js
 for file in `ls -1 *"${USERJSEXT}"`; do
   echo Processing "${file}"...
 
-  tr -d "\r" < "${file}" > "${file}.lf"
+  tr -d "\r" < "${file}" | sed -e 's/ \+$//' > "${file}.lf"
   mv "${file}.lf" "${file}"
 done
 
