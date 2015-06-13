ef130a04723766891a5335cdce8a730e232e33dd
Taichi <taichi@curipha.jp>
2015-06-13 13:58:17 +0900

    Bump version to 0.1.5

--- add_google_sitesearch.user.js
+++ add_google_sitesearch.user.js
@@ -8,7 +8,7 @@
 // @exclude        https://*.google.tld/*
 // @exclude        http://*.amazon.tld/*
 // @exclude        https://*.amazon.tld/*
-// @version        0.1.4
+// @version        0.1.5
 // @grant          GM_addStyle
 // @noframes
 // ==/UserScript==
