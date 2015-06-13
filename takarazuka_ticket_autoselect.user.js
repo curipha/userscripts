f04fe32f76d920f0814799403a31b56cd928645a
Taichi <taichi@curipha.jp>
2015-06-13 12:40:21 +0900

    Add takarazuka_ticket_autoselect.user.js

---
+++ takarazuka_ticket_autoselect.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name           Takarazuka ticket autoselect
+// @namespace      curipha
+// @description    Choose sheet category and number automatically
+// @include        https://ent-kageki.e-tix.jp/ticket/seatnumberselect.html*
+// @version        0.0.1
+// @grant          none
+// @noframes
+// ==/UserScript==
+
+(function(){
+  document.getElementById('tc11').children[1].selected = true;  // Sheet S
+  document.getElementById('tc21').children[1].selected = true;  // Sheet A
+  document.getElementById('tc31').children[1].selected = true;  // Sheet B
+
+  var type = document.body.querySelectorAll('input[name="rt"][value="2"]');
+  for (var input of type) {
+    input.checked = true;
+  }
+})();
+
