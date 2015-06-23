ff292656557e0a9a26d6ec9a63b0813b2a82ee83
Taichi <taichi@curipha.jp>
2015-06-24 00:20:18 +0900

    Code clean-up

--- amazon_personal_filter.user.js
+++ amazon_personal_filter.user.js
@@ -15,46 +15,30 @@
   var opacity = 0.2;
   var pattern = /(無料|立ち?読み|お試し|(試し?|ためし)読|スターター|STARTER|ダイジェスト|雑誌|([^0-9０-９][0０]|創刊)号|[Vv]ol *[．\.] *[0０]([^0-9０-９]|$))/;
 
-  // Search result
-  if (document.getElementById('searchTemplate')) {
-    var opaque_result = function() {
-      var result = document.body.querySelectorAll('#searchTemplate .s-result-item');
+  var opaque = function(list_selector, item_selector) {
+    return function() {
+      var result = document.body.querySelectorAll(list_selector);
       for (var item of result) {
-        var title = item.querySelector('h2');
+        var title = item.querySelector(item_selector);
         if (!title) continue;
         if (pattern.test(title.textContent)) item.style.opacity = opacity;
       }
     };
+  };
 
-    var mo_sr = new MutationObserver(opaque_result);
+  // Search result
+  if (document.getElementById('searchTemplate')) {
+    var mo_sr = new MutationObserver(opaque('#searchTemplate .s-result-item', 'h2'));
     mo_sr.observe(document.body, { childList: true, subtree: true });
   }
   if (document.getElementById('mainResults')) {
-    var opaque_result2 = function() {
-      var result = document.body.querySelectorAll('#mainResults [id^="result_"]');
-      for (var item of result) {
-        var title = item.querySelector('h3 .lrg');
-        if (!title) continue;
-        if (pattern.test(title.textContent)) item.style.opacity = opacity;
-      }
-    };
-
-    var mo_sr2 = new MutationObserver(opaque_result2);
+    var mo_sr2 = new MutationObserver(opaque('#mainResults [id^="result_"]', 'h3 .lrg'));
     mo_sr2.observe(document.body, { childList: true });
   }
 
   // Best sellers
   if (document.getElementById('zg_col1')) {
-    var opaque_bestseller = function() {
-      var result = document.body.querySelectorAll('#zg_col1 .zg_item_compact');
-      for (var item of result) {
-        var title = item.querySelector('.zg_title');
-        if (!title) continue;
-        if (pattern.test(title.textContent)) item.style.opacity = opacity;
-      }
-    };
-
-    var mo_bs = new MutationObserver(opaque_bestseller);
+    var mo_bs = new MutationObserver(opaque('#zg_col1 .zg_item_compact', '.zg_title'));
     mo_bs.observe(document.body, { childList: true });
   }
 
