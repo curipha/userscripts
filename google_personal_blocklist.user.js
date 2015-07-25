a6bb4bc07112905b2143f756643406d9947875d2
Taichi <taichi@curipha.jp>
2015-07-25 23:56:15 +0900

    Bump version to 0.2.3

--- google_personal_blocklist.user.js
+++ google_personal_blocklist.user.js
@@ -4,7 +4,7 @@
 // @description    Obscure annoying sites from search result
 // @include        http://www.google.tld/search?*
 // @include        https://www.google.tld/search?*
-// @version        0.2.2
+// @version        0.2.3
 // @grant          none
 // @noframes
 // ==/UserScript==
