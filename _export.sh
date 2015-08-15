a03aa0ce05f360a17347433a9d34a8fcaab95f88
Taichi <taichi@curipha.jp>
2015-08-15 13:00:01 +0900

    Fix an issue of handling "*" in shell

--- _export.sh
+++ _export.sh
@@ -13,17 +13,14 @@ USERJSEXT=.user.js
 # Change directory to the same level as this script
 cd `dirname "${0}"`
 
-
 # Remove current user.js
-rm -f *"${USERJSEXT}"
-
+rm -f *${USERJSEXT}
 
 # Copy userscripts from Firefox's user profile directory
 find "${FXPROFILE}" -type f -iname "*${USERJSEXT}" -ipath "*/*_scripts/*" -exec cp {} . \;
 
-
-# Change linefeed code to LF only
-for file in `ls -1 *"${USERJSEXT}"`; do
+# Processing each file
+for file in `ls -1 *${USERJSEXT}`; do
   echo Processing "${file}"...
 
   tr -d "\r" < "${file}" | sed -e 's/ \+$//' > "${file}.lf"
