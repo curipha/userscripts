31a7fb2039e72920105a6ead8e5edd756d7d7f4f
Taichi <taichi@curipha.jp>
2015-10-14 22:44:37 +0900

    Bump version to 0.1.0

--- takarazuka_ticket_autoselect.user.js
+++ takarazuka_ticket_autoselect.user.js
@@ -3,7 +3,7 @@
 // @namespace      curipha
 // @description    Choose sheet category and number automatically
 // @include        https://ent-kageki.e-tix.jp/ticket/seatnumberselect.html*
-// @version        0.0.2
+// @version        0.1.0
 // @grant          none
 // @noframes
 // ==/UserScript==
