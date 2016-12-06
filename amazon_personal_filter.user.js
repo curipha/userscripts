f9053561529d95d6fcd88fb1822c7411e9ab3660
Taichi <taichi@curipha.jp>
2016-12-06 21:42:39 +0900

    Follow minor design update of best sellers

--- amazon_personal_filter.user.js
+++ amazon_personal_filter.user.js
@@ -48,7 +48,7 @@
 
   // Best sellers
   if (document.getElementById('zg_col1')) {
-    var mo_bs = new MutationObserver(opaque('#zg_col1 .zg_item_compact', '.a-col-right .a-link-normal[rel]'));
+    var mo_bs = new MutationObserver(opaque('#zg_col1 .zg_item_compact', '.a-col-right .a-link-normal'));
     mo_bs.observe(document.body, { childList: true });
   }
 
