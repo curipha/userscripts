f161615b1ebdc3aab45a654fd0746f982fa9e06b
Taichi <taichi@curipha.jp>
2017-05-20 13:22:28 +0900

    Remove (maybe) obsoleted code
    I've never seen an age-gate for alcohol lately.
    It might have been replaced with the check box on order confirmation screen.
    
    A modal screen for age confirmation for adult products is no longer a common sight as well.

--- turn_off_age_gate_on_Amazon.co.jp.user.js
+++ turn_off_age_gate_on_Amazon.co.jp.user.js
@@ -22,29 +22,4 @@
     location.href = a.href;
     return;
   }
-
-  // Alcohol
-  const input = document.body.querySelector('.dobBody form input[type="image"]');
-  if (input) {
-    input.click();
-    return;
-  }
-
-  // Adult (shown up in delay)
-  if (!location.pathname.includes('/dp/')) {
-    let mo;
-    const clickit_mo = function(mr) {
-      for (let mrl of mr) {
-        for (let i = 0; i < mrl.addedNodes.length; i++) {
-          if (mrl.addedNodes[i].className === 'a-modal-scroller a-declarative') {
-            mo.disconnect();
-            setTimeout(function() { mrl.addedNodes[i].querySelector('[data-sx-lift-black-curtain-action]').click(); }, 120);
-            return;
-          }
-        }
-      }
-    };
-    mo = new MutationObserver(clickit_mo);
-    mo.observe(document.body, { childList: true });
-  }
 })();
