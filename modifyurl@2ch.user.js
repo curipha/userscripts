45b6f2fc040f92d1179bda223da1075177b8506a
Taichi <taichi@curipha.jp>
2017-06-22 23:13:45 +0900

    v0.2.4

--- modifyurl@2ch.user.js
+++ modifyurl@2ch.user.js
@@ -6,7 +6,7 @@
 // @match          https://*.2ch.net/*
 // @match          http://*.bbspink.com/*
 // @match          https://*.bbspink.com/*
-// @version        0.2.3
+// @version        0.2.4
 // @grant          none
 // @noframes
 // ==/UserScript==
