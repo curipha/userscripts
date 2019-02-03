7fa0f4d36ba32560647b02217e7f8a81c54f6dcd
Taichi <taichi@curipha.jp>
2019-02-03 23:11:31 +0900

    v0.5.3

--- turn_off_age_gate_on_amazon.co.jp.user.js
+++ turn_off_age_gate_on_amazon.co.jp.user.js
@@ -9,7 +9,7 @@
 // @exclude        https://www.amazon.co.jp/ap/*
 // @exclude        https://www.amazon.co.jp/mn/*
 // @exclude        https://www.amazon.co.jp/clouddrive*
-// @version        0.5.2
+// @version        0.5.3
 // @grant          none
 // @noframes
 // ==/UserScript==
