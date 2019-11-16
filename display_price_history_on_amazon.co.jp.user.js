d529e1254f43e5df1ad3e8c4b8773132c7c7bdac
Taichi <taichi@curipha.jp>
2019-11-16 20:11:50 +0900

    v0.1.2

--- display_price_history_on_amazon.co.jp.user.js
+++ display_price_history_on_amazon.co.jp.user.js
@@ -8,7 +8,7 @@
 // @exclude        https://www.amazon.co.jp/ap/*
 // @exclude        https://www.amazon.co.jp/hz/*
 // @exclude        https://www.amazon.co.jp/clouddrive*
-// @version        0.1.1
+// @version        0.1.2
 // @grant          none
 // @noframes
 // ==/UserScript==
