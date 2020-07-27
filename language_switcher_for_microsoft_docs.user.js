954f3fc5dd6915116a340694dd45c353c083aadb
Taichi <taichi@curipha.jp>
2020-07-28 00:53:27 +0900

    v0.0.2

--- language_switcher_for_microsoft_docs.user.js
+++ language_switcher_for_microsoft_docs.user.js
@@ -3,7 +3,7 @@
 // @namespace      curipha
 // @description    Add language menu
 // @include        https://docs.microsoft.com/*
-// @version        0.0.1
+// @version        0.0.2
 // @grant          none
 // @noframes
 // ==/UserScript==
